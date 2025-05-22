440e857aba14e30024e0e0acad52db02f988af37
Alex Silva <alex@marknet.com.br>
2025-05-22 17:32:38 -0300

    add filters and loads data from categories and places

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../classes/lugares/lugar';
+import { Categoria } from '../../classes/categoria/categoria';
+import { LugarService } from '../../services/lugares/lugar.service';
+import { CategoriaService } from '../../services/categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +10,22 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
 
+export class GaleriaComponent implements OnInit{
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
+
+  constructor(
+    private lugarService: LugarService, 
+    private categoriaService: CategoriaService
+  ) {}
+
+  ngOnInit() : void {
+    this.categoriaService.obterTodos().subscribe(categorias => this.categoriasFiltro = categorias);
+    this.lugarService.obterTodos().subscribe(lugares => this.lugares = lugares);
+  }
+
+  getTotalEstrelas(lugar: Lugar) : string {
+    return '&#9733;'.repeat(lugar.avaliacao || 0) + '&#9734;'.repeat(5 - (lugar.avaliacao || 0));
+  }
 }
