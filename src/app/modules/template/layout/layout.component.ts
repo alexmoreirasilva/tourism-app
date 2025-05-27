ed08dd87abab135222c143ac19f2ed7cd8e8f385
Alex Silva <alexmoreira64@gmail.com>
2025-05-27 11:07:26 -0300

    Adds dynamic layout titles and subtitles from route data

--- src/app/modules/template/layout/layout.component.ts
+++ src/app/modules/template/layout/layout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutProps } from '../../../@types/layout';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -7,7 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss'
 })
 
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
   currentYear: number = new Date().getFullYear();
-  
+  props: LayoutProps = { title: '', subtitle: ''  };
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.router.events
+    .pipe( filter(() => this.activatedRoute.firstChild !== null), map(() => this.obterPropriedadesLayout()))
+    .subscribe((props: LayoutProps) =>  this.props = props)
+  }
+
+  obterPropriedadesLayout() : LayoutProps {
+    let rotaFilha = this.activatedRoute.firstChild;
+
+    while(rotaFilha?.firstChild) {
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 }
--- src/app/modules/template/template-routing.module.ts
+++ src/app/modules/template/template-routing.module.ts
@@ -3,28 +3,40 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: LayoutComponent, 
+  {
+    path: '',
+    component: LayoutComponent,
     children: [
-      { 
-        path: 'categorias', 
+      {
+        path: 'categorias',
         loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: {
+          title: 'Categorias',
+          subtitle: 'Realize o cadastro de novas categorias'
+        }
       },
 
-      { 
-        path: 'lugares', 
+      {
+        path: 'lugares',
         loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: {
+          title: 'Lugares',
+          subtitle: 'Realize o cadastro de novos lugares'
+        }
       },
 
       {
         path: 'galeria',
         loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: {
+          title: 'Lista de lugares',
+          subtitle: 'Descubra os melhores lugare para visitar'
+        }
       }
-    ] 
+    ]
   }
 ];
 
